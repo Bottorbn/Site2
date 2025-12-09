8915e549beec33134c20d63dcb2433919a535d3c
Bottorbn <bottorbn@gmail.com>
2025-12-09 08:10:52 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("helloButton");
+const message = document.getelementByID("message");
+
+button.addEventListener("click", () => {
+window.open("https://www.youtube.com/watch?v=Aq5WXmQQooo", "_blank");
+});
