9c4886e73051d192ff197b7f1eb2f2a44cc3640e
Bottorbn <bottorbn@gmail.com>
2025-12-09 08:44:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.getElementById("helloButton");
 const message = document.getelementByID("message");
 
 button.addEventListener("click", () => {
-window.open("https://www.youtube.com/watch?v=Aq5WXmQQooo", "_blank");
+window.open("https://www.youtube.com/watch?v=Aq5WXmQQooo", "_self");
 });
